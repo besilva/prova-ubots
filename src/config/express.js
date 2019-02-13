3a7a947004f28f4cb4d1b9716b4578f547459c0a
bernardo silva <silva98.bernardo@gmail.com>
2019-02-13 14:18:00 -0200

    remove cors

--- src/config/express.js
+++ src/config/express.js
@@ -1,14 +1,12 @@
 import express  from 'express';
 import bodyParser from 'body-parser';
-import routes from "../app/routes"
+import routes from "../app/routes";
 import morgan from 'morgan';
-import cors from 'cors';
 import boom from 'express-boom';
 module.exports = function(){
     var app = express();
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
-    app.use(cors());
     app.use(boom());
     app.use(morgan('tiny'));
     app.use('/', routes);
